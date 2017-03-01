a3f5c1ba48436b6280b3f656ee7e053cc73643c3
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-01 13:53:04 +0100

    Calculate pension

---
+++ calc.sh
@@ -0,0 +1,33 @@
+INKOMSTPENSION=$(cat target/inkomstpension.txt)
+PREMIEPENSION=$(cat target/premiepension.txt)
+TJANSTEPENSION=$(cat target/tjanstepension.txt)
+PRIVATPENSION=$(cat target/privatpension.txt)
+TOTALT=$(echo "scale=0; (${INKOMSTPENSION} + ${PREMIEPENSION} + ${TJANSTEPENSION} + ${PRIVATPENSION}) / 1" | bc -l)
+echo "
+<h1>Din pension</h1>
+<table>
+<tbody>
+<tr>
+<td>Inkomstpension</td>
+<td>${INKOMSTPENSION}</td>
+</tr>
+<tr>
+<td>Premiepension</td>
+<td>${PREMIEPENSION}</td>
+</tr>
+<tr>
+<td>Tjänstepension</td>
+<td>${TJANSTEPENSION}</td>
+</tr>
+<tr>
+<td>Privatpension</td>
+<td>${PRIVATPENSION}</td>
+</tr>
+<tr>
+<td>Totalt</td>
+<td>${TOTALT}</td>
+</tr>
+</tbody>
+</table>
+" > target/dinpension.html
+
